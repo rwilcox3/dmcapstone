8650cfce523d33a9a706e90093bd4908eb0b0273
Robert Wilcox <bew@panduit.com>
2021-05-30 14:54:47 -0500

    nhj

--- dendogram/bde1f995226f3be4@197.js
+++ dendogram/bde1f995226f3be4@197.js
@@ -1,7 +1,7 @@
 // https://observablehq.com/@d3/radial-dendrogram@197
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
+  const fileAttachments = new Map([["./flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], function(md){return(
 md`# Radial Dendrogram
